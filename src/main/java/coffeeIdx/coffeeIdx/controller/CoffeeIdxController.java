881eeb43a6d296c641aadb0638a80815b72db0fa
PippasSong <tlmag2108@gmail.com>
2021-04-02 23:58:51 +0900

    로거 추가, 새로운 DTO

--- src/main/java/coffeeIdx/coffeeIdx/controller/CoffeeIdxController.java
+++ src/main/java/coffeeIdx/coffeeIdx/controller/CoffeeIdxController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
@@ -21,17 +23,31 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller //컨트롤러 어노테이션
 public class CoffeeIdxController {
+	//로거 적용
+	private Logger log = LoggerFactory.getLogger(this.getClass());  //로거의 이름에는 보통 클래스 객체를 넘겨준다
+	
+	
 	@Autowired
 	private CoffeeIdxService coffeeIdxService;
 	
 	//요청에 대한 주소, 요청 방법을 지정
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public ModelAndView openCoffeeIdxList() throws Exception{
+		//debug용의 로그를 출력
+		log.debug("openCoffeeIdxList");
 		//뷰
 		ModelAndView mv = new ModelAndView("/coffeeIdx/coffeeList"); //templates 경로
 		
 		List<CoffeeIdxDto> list = coffeeIdxService.selectCoffeeList(); //게시글 목록 조회
 		mv.addObject("list", list); //실행된 비즈니스로직의 결과 값을 뷰에 'list'라는 이름으로 저장
+		
+		return mv;
+	}
+	
+	//@pathvariable 은 메서드의 파라미터가 uri의 변수로 사용되는 것을 의미 cafename라는 이름의 파라미터가 {cafename}에 바인드된다 
+	@RequestMapping(value="/index/{cafeName}", method=RequestMethod.GET)
+	public ModelAndView openCoffeeIdxDetail(@PathVariable("cafeName") String cafeName) throws Exception{
+		ModelAndView mv = new ModelAndView("/coffeeIdx/coffeeDetail");
 		
 		return mv;
 	}
